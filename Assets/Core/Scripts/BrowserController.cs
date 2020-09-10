94e4ae4750a7e0e590ff34958e4167f1b07cbfc6
Oxters <oxters168@gmail.com>
2020-09-10 22:12:21 +0300

    Interface the explorer with the grid creating the browser with its BrowserController script.

---
+++ Assets/Core/Scripts/BrowserController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class BrowserController : MonoBehaviour
+{
+    public Explorer explorer;
+    public Grid grid;
+
+    void Start()
+    {
+        explorer.onDirectoriesChanged += OnExplorerChanged;
+        explorer.onFilesChanged += OnExplorerChanged;
+    }
+
+    private void OnExplorerChanged(Explorer caller)
+    {
+        Debug.Log("Browser changed");
+        var ld = caller.GetLogicalDrives();
+        var dirs = caller.GetDirectories();
+        var files = caller.GetFiles();
+        int ldCount = ld != null ? ld.Length : 0;
+        int dirsCount = dirs != null ? dirs.Length : 0;
+        int filesCount = files != null ? files.Length : 0;
+        FSData[] allData = new FSData[ldCount + dirsCount + filesCount];
+        for (int i = 0; i < ldCount; i++)
+        {
+            var currentLD = new FSData() { fullPath = ld[i], isFile = false };
+            allData[i] = currentLD;
+        }
+        for (int i = 0; i < dirsCount; i++)
+        {
+            var currentDir = new FSData() { fullPath = dirs[i], isFile = false };
+            allData[i + ldCount] = currentDir;
+        }
+        for (int i = 0; i < filesCount; i++)
+        {
+            var currentFile = new FSData() { fullPath = files[i], isFile = true };
+            allData[i + ldCount + dirsCount] = currentFile;
+        }
+
+        grid.SetData(allData);
+    }
+}
--- Assets/Core/Scripts/Explorer.cs
+++ Assets/Core/Scripts/Explorer.cs
@@ -5,12 +5,17 @@ using System;
 
 public class Explorer : MonoBehaviour
 {
+    public delegate void ContentsChangedCallback(Explorer caller);
+    public event ContentsChangedCallback onDirectoriesChanged;
+    public event ContentsChangedCallback onFilesChanged;
+    public event ContentsChangedCallback onLogicalDrivesChanged;
+
     private List<string> pathHistory = new List<string>();
-    public string currentPath { get; private set; }
+    public string currentPath;
     public float refreshTime = 10;
-    private string[] logicalDrives;
-    private string[] directories;
-    private string[] files;
+    public string[] logicalDrives;
+    public string[] directories;
+    public string[] files;
     private float lastRefresh = float.MinValue;
 
     void Update()
@@ -21,15 +26,15 @@ public class Explorer : MonoBehaviour
 
     public string[] GetLogicalDrives()
     {
-        return (string[])logicalDrives.Clone();
+        return (string[])logicalDrives?.Clone();
     }
     public string[] GetDirectories()
     {
-        return (string[])directories.Clone();
+        return (string[])directories?.Clone();
     }
     public string[] GetFiles()
     {
-        return (string[])files.Clone();
+        return (string[])files?.Clone();
     }
     
     public void Goto(string path)
@@ -69,17 +74,66 @@ public class Explorer : MonoBehaviour
     {
         lastRefresh = Time.time;
 
-        logicalDrives = Directory.GetLogicalDrives();
-
+        SetLogicalDrives(Directory.GetLogicalDrives());
+        
         if (string.IsNullOrEmpty(currentPath) || !Directory.Exists(currentPath))
             GoBack();
         if (pathHistory.Count <= 0 || !PathEquals(pathHistory[pathHistory.Count - 1], currentPath))
             pathHistory.Add(currentPath);
-
+        
         //These are kept outside the second if in case there have been changes in the directory
-        directories = Directory.GetDirectories(currentPath);
-        files = Directory.GetFiles(currentPath);
+        SetDirectories(Directory.GetDirectories(currentPath));
+        SetFiles(Directory.GetFiles(currentPath));
     }
+
+    private void SetLogicalDrives(string[] newLogicalDrives)
+    {
+        bool foundChange = ComparePaths(logicalDrives, newLogicalDrives);
+        if (foundChange)
+        {
+            onLogicalDrivesChanged?.Invoke(this);
+            logicalDrives = newLogicalDrives;
+        }
+    }
+    private void SetDirectories(string[] newDirectories)
+    {
+        bool foundChange = ComparePaths(directories, newDirectories);
+        if (foundChange)
+        {
+            onDirectoriesChanged?.Invoke(this);
+            directories = newDirectories;
+        }
+    }
+    private void SetFiles(string[] newFiles)
+    {
+        bool foundChange = ComparePaths(files, newFiles);
+        if (foundChange)
+        {
+            onFilesChanged?.Invoke(this);
+            files = newFiles;
+        }
+    }
+
+    public static bool ComparePaths(string[] first, string[] second)
+    {
+        bool foundChange = false;
+        if (first != null && second != null && first.Length == second.Length)
+        {
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (!PathEquals(first[i], second[i]))
+                {
+                    foundChange = true;
+                    break;
+                }
+            }
+        }
+        else
+            foundChange = true;
+        
+        return foundChange;
+    }
+
     private void ResetPath()
     {
         currentPath = Application.persistentDataPath;
