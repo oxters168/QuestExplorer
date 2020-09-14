4a1078768766b33e73c9919c48a63010cc500425
Oxters <oxters168@gmail.com>
2020-09-15 01:06:43 +0300

    Fixed issue where grid does not follow board properly. Added side grid for logical drives. Fixed an issue where the wrong number of anchors would be created for the grid.

--- Assets/Core/Scripts/BrowserController.cs
+++ Assets/Core/Scripts/BrowserController.cs
@@ -1,10 +1,10 @@
 ﻿using UnityEngine;
-using UnityHelpers;
 
 public class BrowserController : MonoBehaviour
 {
     public Explorer explorer;
-    public Grid grid;
+    public Grid mainGrid;
+    public Grid sideGrid;
 
     void Start()
     {
@@ -20,36 +20,25 @@ public class BrowserController : MonoBehaviour
         int ldCount = ld != null ? ld.Length : 0;
         int dirsCount = dirs != null ? dirs.Length : 0;
         int filesCount = files != null ? files.Length : 0;
-        FSData[] allData = new FSData[ldCount + dirsCount + filesCount];
+        FSData[] mainData = new FSData[dirsCount + filesCount];
+        FSData[] sideData = new FSData[ldCount];
         for (int i = 0; i < ldCount; i++)
         {
             var currentLD = new FSData(ld[i], false, explorer);
-            allData[i] = currentLD;
+            sideData[i] = currentLD;
         }
         for (int i = 0; i < dirsCount; i++)
         {
             var currentDir = new FSData(dirs[i], false, explorer);
-            allData[i + ldCount] = currentDir;
+            mainData[i] = currentDir;
         }
         for (int i = 0; i < filesCount; i++)
         {
             var currentFile = new FSData(files[i], true, explorer);
-            allData[i + ldCount + dirsCount] = currentFile;
+            mainData[i + dirsCount] = currentFile;
         }
 
-        var items = grid.SetData(allData);
-        // for (int i = 0; i < items.Length; i++)
-        // {
-        //     var pt = items[i].GetComponent<PhysicsTransform>();
-        //     if (pt != null)
-        //     {
-        //         var lp = items[i].localPosition;
-        //         var lr = items[i].localRotation;
-        //         pt.parent = items[i].parent;
-        //         pt.localPosition = lp;
-        //         pt.localRotation = lr;
-        //         items[i].SetParent(null);
-        //     }
-        // }
+        mainGrid.SetData(mainData);
+        sideGrid.SetData(sideData);
     }
 }
--- Assets/Core/Scripts/Grid/Grid.cs
+++ Assets/Core/Scripts/Grid/Grid.cs
@@ -1,20 +1,29 @@
 ﻿using UnityEngine;
 using UnityHelpers;
+using System.Collections.Generic;
 
 public class Grid : MonoBehaviour
 {
+    public Transform gridItemPrefab;
+    private ObjectPool<Transform> GridItemsPool { get { if (_gridItemsPool == null) _gridItemsPool = new ObjectPool<Transform>(gridItemPrefab, 5, false, true, transform); return _gridItemsPool; } }
+    private ObjectPool<Transform> _gridItemsPool;
     public float itemSize = 0.2f;
     public float padding = 0.1f;
     public int gridSizeX = 3;
     public int gridSizeY = 3;
+    public float itemsPosY = 0.1f;
     private int page = 0;
     private GridItemData[] items;
+    private List<Transform> itemAnchors = new List<Transform>();
     
-    public Transform[] SetData(params GridItemData[] data)
+    public void SetData(int pageNum, params GridItemData[] data)
     {
         items = (GridItemData[])data.Clone();
-        page = 0;
-        return Refresh();
+        SetPage(pageNum);
+    }
+    public void SetData(params GridItemData[] data)
+    {
+        SetData(0, data);
     }
     public int GetPageCount()
     {
@@ -27,7 +36,7 @@ public class Grid : MonoBehaviour
     }
     public void SetPage(int index)
     {
-        page = Mathf.Clamp(index, 0, GetPageCount() - 1);
+        page = Mathf.Clamp(index, 0, Mathf.Clamp(GetPageCount() - 1, 0, int.MaxValue));
         Refresh();
     }
     public int GetPage()
@@ -44,31 +53,54 @@ public class Grid : MonoBehaviour
         SetPage(page - 1);
         Refresh();
     }
-    private Transform[] Refresh()
+    private void Refresh()
     {
-        var itemsPool = PoolManager.GetPool("Items");
-        itemsPool.ReturnAll();
-        //itemsPool.SetParent(transform);
+        GridItemsPool.ReturnAll();
         int itemsCount = items != null ? items.Length : 0;
-        Transform[] itemTransforms = new Transform[itemsCount];
         int itemsPerPage = gridSizeX * gridSizeY;
         int startIndex = itemsPerPage * page;
+
+        int anchorAdditions = itemsPerPage - itemAnchors.Count;
+        for (int i = 0; i < anchorAdditions; i++)
+        {
+            var currentAnchor = new GameObject("Anchor");
+            currentAnchor.transform.SetParent(transform);
+            itemAnchors.Add(currentAnchor.transform);
+        }
         for (int i = startIndex; i < Mathf.Min(startIndex + itemsPerPage, itemsCount); i++)
         {
-            Transform item = itemsPool.Get();
+            Transform item = GridItemsPool.Get();
             item.SetParent(transform);
-            int colIndex = (i - startIndex) % gridSizeX;
-            int rowIndex = (i - startIndex) / gridSizeY;
+            int properIndex = (i - startIndex);
+            int colIndex = 0;
+            int rowIndex = 0;
+            if (gridSizeX > 1 && gridSizeY > 1)
+            {
+                colIndex = properIndex % gridSizeX;
+                rowIndex = properIndex / gridSizeY;
+            }
+            else if (gridSizeX <= 1)
+            {
+                rowIndex = properIndex;
+            }
+            else
+            {
+                colIndex = properIndex;
+            }
+
             float startHor = -(itemSize * gridSizeX + padding * (gridSizeX - 1)) / 2 + itemSize / 2;
             float startVer = (itemSize * gridSizeY + padding * (gridSizeY - 1)) / 2 - itemSize  / 2;
-            item.localPosition = new Vector3(startHor + colIndex * (itemSize + padding), 0, startVer - rowIndex * (itemSize + padding));
-            itemTransforms[i] = item;
+            var gridPos = new Vector3(startHor + colIndex * (itemSize + padding), itemsPosY, startVer - rowIndex * (itemSize + padding));
+            var anchor = itemAnchors[properIndex];
+            anchor.localPosition = gridPos;
+            anchor.localRotation = Quaternion.identity;
+            item.localPosition = gridPos;
+            var mimic = item.GetComponentInParent<MimicTransform>();
+            mimic.other = anchor;
 
             var gridItem = item.GetComponent<IGridItem>();
             gridItem.SetSize(itemSize);
             gridItem.SetData(items[i]);
         }
-
-        return itemTransforms;
     }
 }
